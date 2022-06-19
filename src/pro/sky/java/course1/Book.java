ab9b294bc38144349a6236acdac917f2f133d936
Vero0102 <104280568+Vero0102@users.noreply.github.com>
2022-06-19 23:16:30 +0300

    Homework 1.11 is ended

---
+++ src/pro/sky/java/course1/Book.java
@@ -0,0 +1,54 @@
+package pro.sky.java.course1;
+
+import java.util.Objects;
+
+public class Book {
+    private final String title;
+    private final Author author;
+    private int yearOfPublication;
+
+    public Book(String title, Author author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Издание: " +'\"' + title + '\"' + ". " + author + " Год издания: " + yearOfPublication + ".";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, yearOfPublication);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return yearOfPublication == book.getYearOfPublication() && title.equals(book.getTitle()) && author.equals(book.getAuthor());
+    }
+}
+
--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -3,6 +3,40 @@ package pro.sky.java.course1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Author authorCardNumber1 = new Author("Габриэль Гарсиа", "Маркес"); // автор1
+        Author authorCardNumber2 = new Author("Кэти", "Сьерра"); // автор2
+        Author authorCardNumberX = new Author("Кэти", "Сьерра"); // копия_карточки_автора2
+
+        Book bookCardNumber1 = new Book("Сто лет одиночества", authorCardNumber1, 1967); // оригинал1
+        Book bookCardNumber2 = new Book("Изучаем Java", authorCardNumber2, 2003); // оригинал2
+        Book bookCardNumberX = new Book("Изучаем Java", authorCardNumberX, 2021); // копия2
+
+        bookCardNumber2.setYearOfPublication(2021);
+
+        System.out.println(authorCardNumber1); //автор1
+        System.out.println(authorCardNumber2); //автор2
+        System.out.println(authorCardNumberX); // копия_карточки_автора2
+
+        System.out.println(bookCardNumber1); //оригинал1
+        System.out.println(bookCardNumber2); //оригинал2
+        System.out.println(bookCardNumberX); // копия2
+
+        System.out.println(authorCardNumber1.hashCode()); //хеш автора1
+        System.out.println(authorCardNumber2.hashCode()); //хеш автора2
+        System.out.println(authorCardNumberX.hashCode()); //хеш копии_карточки_автора2
+
+        System.out.println(authorCardNumber1.equals(authorCardNumber2)); //автор1 == автор2
+        System.out.println(authorCardNumber1.equals(authorCardNumberX)); // автор1 == копия_карточки_автора2
+        System.out.println(authorCardNumber2.equals(authorCardNumberX)); // автор2 == копия_карточки_автора2
+
+
+        System.out.println(bookCardNumber1.hashCode()); //хеш оригинала1
+        System.out.println(bookCardNumber2.hashCode()); //хеш оригинала2
+        System.out.println(bookCardNumberX.hashCode()); //хеш копии2
+
+        System.out.println(bookCardNumber1.equals(bookCardNumber2)); //оригинал1 == оригинал2
+        System.out.println(bookCardNumber1.equals(bookCardNumberX)); //оригинал1 == копия2
+        System.out.println(bookCardNumber2.equals(bookCardNumberX)); //оригинал2 == копия2
+
     }
 }
